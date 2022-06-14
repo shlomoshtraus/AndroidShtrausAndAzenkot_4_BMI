71e49b620dc8f32f9d434d895e81cbe46ca1885d
Shloimy <Shloymi987@gmail.com>
2022-06-14 14:08:09 +0300

    second commit

--- app/src/main/java/com/example/androidshtrausandazenkot_4_bmi/Body_information.java
+++ app/src/main/java/com/example/androidshtrausandazenkot_4_bmi/Body_information.java
@@ -2,20 +2,70 @@ package com.example.androidshtrausandazenkot_4_bmi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import com.google.android.material.slider.Slider;
+
 
 public class Body_information extends AppCompatActivity {
 
-
+    Intent intent;
+    RadioButton getSmall, getMedium, getLarge;
+    Double slimness;
+    EditText weight;
+    Slider height;
+    Button submit;
+    RadioGroup group;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_body_information);
 
+        getSmall = findViewById(R.id.small);
+        getMedium = findViewById(R.id.medium);
+        getLarge = findViewById(R.id.large);
+        height = findViewById(R.id.heightSlider);
+        weight = findViewById(R.id.actual_weight_input);
+        group = findViewById(R.id.body_frame_group);
+        submit = findViewById(R.id.submit);
+        intent = getIntent();
+
+        submit.setOnClickListener(e -> {
+            //in case the user didn't put any data in the text field
+            if (weight.getText().toString().equals(""))
+                Toast.makeText(this, "Fill in all the required fields!",
+                        Toast.LENGTH_SHORT).show();
+            else {
+                //setting up the 'slimness' variable
+                slimness = findSelectedRadioButton(group.getCheckedRadioButtonId());
+                //pass the data field to the ResultActivity activity.
+                intent.putExtra("height", (int)height.getValue());
+                intent.putExtra("slimness", slimness);
+                intent.putExtra("weight", Double.parseDouble(weight.getText().toString()));
+                intent.setClass(this,Result.class);
+                startActivity(intent);
+                //closing the current activity
+                finish();
+
+            }
+        });
 
 
+    }
+
+    private Double findSelectedRadioButton(int checkButtonId) {
+        //getting the button that is benn checked
+        if (checkButtonId == R.id.small) {
+            return 0.9;
+        } else if (checkButtonId == R.id.medium) {
+            return 1.0;
+        } else
+            return 1.1;
     }
 }
--- app/src/main/java/com/example/androidshtrausandazenkot_4_bmi/Result.java
+++ app/src/main/java/com/example/androidshtrausandazenkot_4_bmi/Result.java
@@ -4,15 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class Result extends AppCompatActivity {
     int age;
     double weight, bmi, idealWeight, height, slimness;
     String status;
-    TextView bmiResult, statusResult, idealWeightResult, MessageDetails;
+    TextView bmiResult, statusResult, idealWeightResult;
     Button mainButton;
 
     @SuppressLint("SetTextI18n")
@@ -36,9 +39,9 @@ public class Result extends AppCompatActivity {
         //calculation the ideal weight.
         idealWeight = ((height * 100) - 100 + (age / 10.0)) * 0.9 * slimness;
 
+        bmiResult.setText(""+ new DecimalFormat("##.###").format(bmi));
         updateWeightStatus(bmi);
-        bmiResult.setText(""+ bmi);
-        idealWeightResult.setText("" + idealWeight);
+        idealWeightResult.setText("" + new DecimalFormat("##.###").format(idealWeight));
         statusResult.setText(status);
 
 
@@ -54,17 +57,29 @@ public class Result extends AppCompatActivity {
     }
     private void updateWeightStatus(double bmiValue) {
         //calculation the weight status based on the bmi.
-        if (bmiValue < 15)
+        if (bmiValue < 15){
             status = "Anorexic";
-        else if (15 <= bmiValue && bmiValue < 18.5)
+            bmiResult.setTextColor(Color.RED);
+            statusResult.setTextColor(Color.RED);}
+        else if (15 <= bmiValue && bmiValue < 18.5){
             status = "Underweight";
-        else if (18.5 <= bmiValue && bmiValue < 25)
+            bmiResult.setTextColor(Color.RED);
+            statusResult.setTextColor(Color.RED);}
+        else if (18.5 <= bmiValue && bmiValue < 25){
             status = "Normal";
-        else if (25 <= bmiValue && bmiValue < 30)
+            bmiResult.setTextColor(Color.BLUE);
+            statusResult.setTextColor(Color.BLUE);}
+        else if (25 <= bmiValue && bmiValue < 30){
             status = "Overweight";
-        else if (30 <= bmiValue && bmiValue < 35)
+            bmiResult.setTextColor(Color.RED);
+            statusResult.setTextColor(Color.RED);}
+        else if (30 <= bmiValue && bmiValue < 35){
             status = "Obese";
-        else if (35 <= bmiValue)
+            bmiResult.setTextColor(Color.RED);
+            statusResult.setTextColor(Color.RED);}
+        else if (35 <= bmiValue){
             status = "Extreme Obese";
+            bmiResult.setTextColor(Color.RED);
+            statusResult.setTextColor(Color.RED);}
     }
 }
